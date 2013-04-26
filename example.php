1ff5cdab9b19d128a6e1495e5dd9df95cab61f39
kafene <matt@kafene.org>
2013-04-26 10:31:59 -0300

    Update example.php

--- example.php
+++ example.php
@@ -29,3 +29,39 @@ $p('childClass::getInstance() === childClass::getInstance()');
 $p('childClass::getInstance() !== parentClass::getInstance()');
 $p('parentClass::getInstance() === parentClass::getInstance()');
 
+/*
+
+Expected Results:
+
+parentClass::getInstance(), childClass::getInstance()
+
+object(parentClass)[5]
+object(childClass)[6]
+$parent, $child
+
+object(parentClass)[1]
+object(childClass)[2]
+$parent->sayName(), $child->sayName()
+
+string 'I am the parent class' (length=21)
+string 'I am the child class' (length=20)
+parentClass::getInstance("bob")
+
+object(parentClass)[7]
+childClass::getInstance("tommy")
+
+object(childClass)[8]
+childClass::getInstance() !== childClass::getInstance("yaya")
+
+boolean true
+childClass::getInstance() === childClass::getInstance()
+
+boolean true
+childClass::getInstance() !== parentClass::getInstance()
+
+boolean true
+parentClass::getInstance() === parentClass::getInstance()
+
+boolean true
+
+*/
